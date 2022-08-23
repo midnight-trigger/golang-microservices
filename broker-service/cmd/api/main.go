232bcd7b6b4c6ab59a1596e6435e171cfd238ec5
西脇潤哉 <amities@junya.local>
2022-08-23 20:18:22 +0900

    second commit

--- broker-service/cmd/api/main.go
+++ broker-service/cmd/api/main.go
@@ -1,5 +1,28 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
 
+const webPort = "80"
+
+type Config struct{}
+
+func main() {
+	app := Config{}
+
+	log.Printf("starting broker service on port %s", webPort)
+
+	// define http server
+	srv := http.Server{
+		Addr:    fmt.Sprintf(":%s", webPort),
+		Handler: app.routes(),
+	}
+
+	// start the server
+	if err := srv.ListenAndServe(); err != nil {
+		log.Panic(err)
+	}
 }
--- broker-service/go.mod
+++ broker-service/go.mod
@@ -2,4 +2,7 @@ module go-micro/broker-service
 
 go 1.19
 
-require github.com/go-chi/chi/v5 v5.0.7
+require (
+	github.com/go-chi/chi/v5 v5.0.7
+	github.com/go-chi/cors v1.2.1
+)
